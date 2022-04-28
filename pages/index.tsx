691e7313b39b5b7a717c2e5e6b9a3113fa9f0812
Piperapk <piperapk@gmail.com>
2022-04-28 18:57:31 +0100

    SEO updated. Copy updated.

--- pages/index.tsx
+++ pages/index.tsx
@@ -19,8 +19,14 @@ const Home: NextPage = () => {
   return (
     <div>
       <Head>
-        <title>Ivan Krushkov - Developer</title>
-        <meta name="description" content="" />
+        <title>Ivan Krushkov - Software Developer - Personal Portfolio and Work</title>
+        <meta name="description" content="I use Engineering and Business Development to create long-term value through technology." />
+
+        <meta property="og:url" content="http://ikrushkov.com/"/>
+        <meta property="og:title" content="Ivan Krushkov - Software Developer - Personal Portfolio and Work" />
+        <meta property="og:description" content="I use Engineering and Business Development to create long-term value through technology."/>
+        <meta property="og:image" content={`https://www.ikrushkov.com/_next/image?url=%2Fmedia%2Fprofile_picture_500x.jpg&w=256&q=75`}/>
+
         <link rel="icon" href="/favicon.svg" />
       </Head>
       <div>
