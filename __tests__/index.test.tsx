80b235ce4f2f26436302df287e5cf5ea26647f7f
Piperapk <piperapk@gmail.com>
2022-07-01 23:10:58 +0100

    Easter egg test in progress

--- __tests__/index.test.tsx
+++ __tests__/index.test.tsx
@@ -1,8 +1,9 @@
-import { render, screen, fireEvent } from "@testing-library/react";
-import AboutMe from "../components/AboutMe";
+import { render, screen, fireEvent, waitFor } from "@testing-library/react";
 
 //Components to test
-import Word_Card, { Props as WorkCardProps } from "../components/cards/Work_Card"
+import Word_Card, { Props as WorkCardProps } from "../components/cards/Work_Card";
+import HeroPage from "../components/HeroPage";
+import AboutMe from "../components/AboutMe";
 
 const defaultProps: WorkCardProps = {
     title: 'Card title',
@@ -25,7 +26,7 @@ describe('Work Cards', () => {
     it('Renders Work_Card component', () => {
         render(<Word_Card {...defaultProps}/>);
 
-        //screen.debug();
+        // screen.debug();
     })
 })
 
@@ -33,12 +34,28 @@ describe('Dark/Light mode', () => {
     it('tests if theme transitions', () => {
         render(<AboutMe />)
 
-        screen.debug();
+        // screen.debug();
 
         const switches = screen.getAllByRole('img', {name: 'Dark, light mode switch'});
         fireEvent(switches[0], new MouseEvent ('click'));
 
-        screen.debug();
+        // screen.debug();
+
+    })
+})
+
+describe('Easter eggs', () => {
+    it('shows easter egg on click', async () => {
+        render(<HeroPage />)
+
+        // screen.debug();
+
+        const easterEggActivation = screen.getByText(/welcome/);
+        fireEvent(easterEggActivation, new MouseEvent ('mouseOver'));
+
+        await waitFor(() => {
+            screen.debug();
+        });
 
     })
 })
