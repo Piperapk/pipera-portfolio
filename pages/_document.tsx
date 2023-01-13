427fe68a8031e6668e82f53a163b1c8ec932ad37
Piperapk <piperapk@gmail.com>
2023-01-13 19:29:34 +0000

    Font changed to Source Sans Pro

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -11,7 +11,7 @@ class MyDocument extends Document {
     return (
       <Html className="scroll-smooth" lang='en'>
         <Head>
-        </Head>
+          <link href="https://fonts.googleapis.com/css2?family=Source+Sans+Pro:ital,wght@0,200;0,300;0,400;0,600;0,700;0,900;1,200;1,300;1,400;1,600;1,700;1,900&display=swap" rel="stylesheet" />        </Head>
         <body className='transition-colors duration-300 bg-white dark:bg-slate-800'>
           <Main />
           <NextScript />
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,4 @@
-const defaultTheme = require('tailwindcss/defaultTheme')
+const defaultTheme = require("tailwindcss/defaultTheme");
 
 module.exports = {
   content: [
@@ -9,58 +9,60 @@ module.exports = {
   theme: {
     extend: {
       colors: {
-        primary: '#297AFF',
-        secondary: '#516c98'
+        primary: "#297AFF",
+        secondary: "#516c98",
       },
       boxShadow: {
-        'md-top': '0 -2.5px 6px -1px rgb(0 0 0 / 0.05), 0 2px 4px -2px rgb(0 0 0 / 0.05)',
-        'md-center': '0 1px 6px 1px rgb(0 0 0 / 0.1), 0 2px 4px -2px rgb(0 0 0 / 0.1)'
+        "md-top":
+          "0 -2.5px 6px -1px rgb(0 0 0 / 0.05), 0 2px 4px -2px rgb(0 0 0 / 0.05)",
+        "md-center":
+          "0 1px 6px 1px rgb(0 0 0 / 0.1), 0 2px 4px -2px rgb(0 0 0 / 0.1)",
       },
       screens: {
-        'containerWidth': '896px',
-        'hh': {'raw': '(hover: hover)'},
-        'pf': {'raw': '(pointer: fine)'}
+        containerWidth: "896px",
+        hh: { raw: "(hover: hover)" },
+        pf: { raw: "(pointer: fine)" },
       },
       fontFamily: {
-        //'sans': ['Selawik', ...defaultTheme.fontFamily.sans],
+        sans: ["Source Sans Pro", ...defaultTheme.fontFamily.sans],
       },
       animation: {
-        'tooltip': 'fade-in-out 3s ease-in infinite',
-        'fade-in': 'fade-in 0.5s ease-in',
-        'fade-in-fast': 'fade-in 0.25s',
-        'fade-out': 'fade-out 0.5s ease-in forwards',
-        'slide': 'slide-in 0.5s',
-        'breathe': 'breathe 6s infinite'
+        tooltip: "fade-in-out 3s ease-in infinite",
+        "fade-in": "fade-in 0.5s ease-in",
+        "fade-in-fast": "fade-in 0.25s",
+        "fade-out": "fade-out 0.5s ease-in forwards",
+        slide: "slide-in 0.5s",
+        breathe: "breathe 6s infinite",
       },
       keyframes: {
-        'fade-in-out': {
-          '0%': {opacity: '0', filter: 'blur(12px)'},
-          '3%': {opacity: '100', filter: 'blur(0)'},
-          '50%': {opacity: '100', filter: 'blur(0)'},
-          '95%': {opacity: '100', filter: 'blur(0)'},
-          '100%': {opacity: '0', filter: 'blur(12px)'}
+        "fade-in-out": {
+          "0%": { opacity: "0", filter: "blur(12px)" },
+          "3%": { opacity: "100", filter: "blur(0)" },
+          "50%": { opacity: "100", filter: "blur(0)" },
+          "95%": { opacity: "100", filter: "blur(0)" },
+          "100%": { opacity: "0", filter: "blur(12px)" },
         },
-        'fade-in': {
-          '0%': {opacity: '0'},
-          '100%': {opacity: '100'}
+        "fade-in": {
+          "0%": { opacity: "0" },
+          "100%": { opacity: "100" },
         },
-        'fade-out': {
-          '0%': {opacity: '100'},
-          '100%': {opacity: '0'}
+        "fade-out": {
+          "0%": { opacity: "100" },
+          "100%": { opacity: "0" },
         },
-        'slide-in': {
-          '0%': {transform: 'translateX(0px)', opacity: '100'},
-          '90%': {transform: 'translateX(175px) scale(0)', opacity: '0'},
-          '100%': {transform: 'translateX(0) scale(1)', opacity: '100'}
+        "slide-in": {
+          "0%": { transform: "translateX(0px)", opacity: "100" },
+          "90%": { transform: "translateX(175px) scale(0)", opacity: "0" },
+          "100%": { transform: "translateX(0) scale(1)", opacity: "100" },
         },
-        'breathe': {
-          '0%': {transform: 'rotate(1.5deg) scale(1)',opacity: '0.75'},
-          '50%': {transform: 'rotate(-1.5deg) scale(1.1)',opacity: '0.5'},
-          '100%': {transform: 'rotate(1.5deg) scale(1)',opacity: '0.75'}
-        }
-      }
+        breathe: {
+          "0%": { transform: "rotate(1.5deg) scale(1)", opacity: "0.75" },
+          "50%": { transform: "rotate(-1.5deg) scale(1.1)", opacity: "0.5" },
+          "100%": { transform: "rotate(1.5deg) scale(1)", opacity: "0.75" },
+        },
+      },
     },
   },
-  darkMode: 'class',
-  plugins: []
-}
+  darkMode: "class",
+  plugins: [],
+};
