e1e8897e3034bc014668da71b921f0c99f0ecba1
Piperapk <piperapk@gmail.com>
2024-03-15 21:55:58 +0000

    updated work entries

--- components/HeroPage.tsx
+++ components/HeroPage.tsx
@@ -48,6 +48,7 @@ const HeroPage = ({ containerWidth }: Props) => {
             <div className='text-lg text-slate-500 dark:text-gray-300 leading-tight font-light'>
               <h4>Web Developer</h4>
               <h4>Cambridge, UK</h4>
+              <h4 className="text-primary"><a href="https://cambridge-intelligence.com/">Cambridge Intelligence</a></h4>
             </div>
           </div>
           <div className='flex pt-2 space-x-1.5 justify-center sm:justify-start'>
