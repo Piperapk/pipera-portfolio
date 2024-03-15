88475d46b3d3d93c30589e13e6142fced5c2b27e
Piperapk <piperapk@gmail.com>
2024-03-15 22:22:01 +0000

    static page gen

--- next.config.js
+++ next.config.js
@@ -3,23 +3,26 @@ const nextConfig = {
   reactStrictMode: true,
   //SVGR
   webpack(config) {
-    config.module.rules.push(      {
-      test: /\.svg$/i,
-      type: 'asset',
-      resourceQuery: /url/, // *.svg?url
-    },
-    {
-      test: /\.svg$/i,
-      issuer: /\.[jt]sx?$/,
-      resourceQuery: { not: [/url/] }, // exclude react component if *.svg?url
-      use: ['@svgr/webpack'],
-    })
+    config.module.rules.push(
+      {
+        test: /\.svg$/i,
+        type: "asset",
+        resourceQuery: /url/, // *.svg?url
+      },
+      {
+        test: /\.svg$/i,
+        issuer: /\.[jt]sx?$/,
+        resourceQuery: { not: [/url/] }, // exclude react component if *.svg?url
+        use: ["@svgr/webpack"],
+      }
+    );
 
-    return config
+    return config;
   },
   images: {
-    domains: ['res.cloudinary.com'],
-  }
-}
+    domains: ["res.cloudinary.com"],
+  },
+  staticPageGenerationTimeout: 300,
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
